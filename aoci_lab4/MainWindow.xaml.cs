1575acaac4511aa0bde2d84492635f9025d98dbd
Сергей Голованчиков <golovan4ickoff@gmail.com>
2025-10-04 21:43:33 +0700

    Перенесены функции из предыдущего проекта

--- aoci_lab4/MainWindow.xaml.cs
+++ aoci_lab4/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,109 @@ namespace aoci_lab4
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Image<Bgr, byte> sourceImage;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public BitmapSource ToBitmapSource(Image<Bgr, byte> image)
+        {
+            var mat = image.Mat;
+
+            return BitmapSource.Create(
+                mat.Width,
+                mat.Height,
+                96d,
+                96d,
+                PixelFormats.Bgr24,
+                null,
+                mat.DataPointer,
+                mat.Step * mat.Height,
+                mat.Step);
+        }
+        public Image<Bgr, byte> ToEmguImage(BitmapSource source)
+        {
+            if (source == null) return null;
+
+            FormatConvertedBitmap safeSource = new FormatConvertedBitmap();
+            safeSource.BeginInit();
+            safeSource.Source = source;
+            safeSource.DestinationFormat = PixelFormats.Bgr24;
+            safeSource.EndInit();
+
+            Image<Bgr, byte> resultImage = new Image<Bgr, byte>(safeSource.PixelWidth, safeSource.PixelHeight);
+            var mat = resultImage.Mat;
+
+            safeSource.CopyPixels(
+                new System.Windows.Int32Rect(0, 0, safeSource.PixelWidth, safeSource.PixelHeight),
+                mat.DataPointer,
+                mat.Step * mat.Height,
+                mat.Step);
+
+            return resultImage;
+        }
+
+        private void LoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Файлы изображений (*.jpg, *.jpeg, *.png)|*.jpg;*.jpeg;*.png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                sourceImage = new Image<Bgr, byte>(openFileDialog.FileName);
+
+                MainImage.Source = ToBitmapSource(sourceImage);
+            }
+        }
+
+        private void SaveImage_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapSource currentWpfImage = MainImage.Source as BitmapSource;
+            if (currentWpfImage == null)
+            {
+                MessageBox.Show("Отсутсвует изображение");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG Image (*.png)|*.png|JPEG Image (*.jpg)|*.jpg|Bitmap Image (*.bmp)|*.bmp";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    Image<Bgr, byte> imageToSave = ToEmguImage(currentWpfImage);
+                    imageToSave.Save(saveFileDialog.FileName);
+
+                    MessageBox.Show($"Изображение успешно сохранено в {saveFileDialog.FileName}");
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show($"Ошибка! Не могу сохранить файл. Подробности: {ex.Message}");
+                }
+            }
+        }
+
+        private void UpdateImage_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapSource currentWpfImage = MainImage.Source as BitmapSource;
+
+            if (currentWpfImage == null)
+            {
+                MessageBox.Show("Изображение отсутсвует");
+                return;
+            }
+
+            sourceImage = ToEmguImage(currentWpfImage);
+            MessageBox.Show("Изменения применены. Теперь это новый оригинал.");
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            if (sourceImage == null) return;
+            MainImage.Source = ToBitmapSource(sourceImage);
         }
     }
 }
